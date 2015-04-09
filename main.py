1a11940946ecb77bb39440c2628cb09e344f3d0a
Paweł Jan Czochański <czochanski@gmail.com>
2015-04-09 20:56:27 +0200

    Better implementation.

--- main.py
+++ main.py
@@ -5,12 +5,34 @@ import json
 import serial
 from concurrent.futures import ThreadPoolExecutor
 
+
 class Server:
+
+    def __init__(self, serial_reader):
+        self._serial_reader = serial_reader
+
+    def __call__(self, websocket, path):
+        data = None
+        while True:
+
+            if not websocket.open:
+                break
+
+            newdata = yield from self._serial_reader.data
+            if newdata != data:
+                data = newdata
+                yield from websocket.send(json.dumps(data))
+                print("Sent {}".format(data))
+
+            yield from asyncio.sleep(0.5)
+
+
+class SerialReader:
 
     def __init__(self, device, baudrate, loop):
         self._connection = serial.Serial(device, baudrate)
         self._loop = loop
-        self._lock = asyncio.Lock()
+
         self._event = asyncio.Event()
         self._event.set()
         self._data = {
@@ -18,42 +40,39 @@ class Server:
             'temperature': 0,
         }
 
+    @property
+    def data(self):
+        yield from self._event.wait()
+        return self._data
+
     @staticmethod
-    def get_serial_data(connection):
+    def _get_serial_data(connection):
         return connection.readline()
 
-    def __call__(self, websocket, path):
+    def __call__(self):
         while True:
-
-            if not websocket.open:
-                break
-
-            if not self._lock.locked() and self._connection.inWaiting() >= 5:
-                with (yield from self._lock):
-                    with ThreadPoolExecutor(max_workers=1) as executor:
-                        serial_data = yield from self._loop.run_in_executor(executor, self.get_serial_data, self._connection)
+            if self._connection.inWaiting() >= 5:
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                    serial_data = yield from self._loop.run_in_executor(
+                        executor, self._get_serial_data, self._connection)
                     if len(serial_data) >= 5:
                         serial_data = serial_data.rstrip().decode('utf-8').split(';')
-                        print("Recived {}".format(serial_data))
+                        print("Read {}".format(serial_data))
                         self._event.clear()
                         self._data = {
                             'insolation': serial_data[0],
                             'temperature': serial_data[1],
                         }
                         self._event.set()
-
-            self._event.wait()  # Wait until the new data is ready
-            yield from websocket.send(json.dumps(self._data))
-            print("Sent {}".format(self._data))
-            diode = yield from websocket.recv()
-
-            yield from asyncio.sleep(2)
+            yield from asyncio.sleep(1)
 
 
 if __name__ == '__main__':
-    server = asyncio.coroutine(Server('/dev/ttyACM0', 9600, asyncio.get_event_loop()))
-
+    serial_reader = SerialReader('/dev/ttyACM0', 9600, asyncio.get_event_loop())
+    server = asyncio.coroutine(Server(serial_reader))
+    serial_reader = asyncio.coroutine(serial_reader)
     start_server = websockets.serve(server, 'localhost', 8889)
 
     asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_until_complete(serial_reader())
     asyncio.get_event_loop().run_forever()
